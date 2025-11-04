c632d1ef827c297766d88e1259851ed06fb728a6
GiuseppeTuccilli <giuseppetuccilli@gmail.com>
2025-11-04 14:57:48 +0100

    Initial commit

---
+++ src/main/java/giuseppetuccilli/Capstone/Gestionale/Immobili/CapstoneGestionaleImmobiliApplication.java
@@ -0,0 +1,13 @@
+package giuseppetuccilli.Capstone.Gestionale.Immobili;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapstoneGestionaleImmobiliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CapstoneGestionaleImmobiliApplication.class, args);
+	}
+
+}
---
+++ src/test/java/giuseppetuccilli/Capstone/Gestionale/Immobili/CapstoneGestionaleImmobiliApplicationTests.java
@@ -0,0 +1,13 @@
+package giuseppetuccilli.Capstone.Gestionale.Immobili;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapstoneGestionaleImmobiliApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
