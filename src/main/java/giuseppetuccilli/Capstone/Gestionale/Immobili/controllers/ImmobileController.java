6e54587d2c388a40bd4a17168ffa6fb6098e4a05
GiuseppeTuccilli <giuseppetuccilli@gmail.com>
2025-11-07 17:24:14 +0100

    crud immobile

---
+++ src/main/java/giuseppetuccilli/Capstone/Gestionale/Immobili/controllers/ImmobileController.java
@@ -0,0 +1,54 @@
+package giuseppetuccilli.Capstone.Gestionale.Immobili.controllers;
+
+import giuseppetuccilli.Capstone.Gestionale.Immobili.entities.Immobile;
+import giuseppetuccilli.Capstone.Gestionale.Immobili.exceptions.ValidazioneFallitaExeption;
+import giuseppetuccilli.Capstone.Gestionale.Immobili.payloads.requests.NewImmoPayload;
+import giuseppetuccilli.Capstone.Gestionale.Immobili.services.ImmobileService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/immobili")
+public class ImmobileController {
+    @Autowired
+    private ImmobileService immobileService;
+
+    //dettagli immobile
+    @GetMapping("/{id}")
+    public Immobile getIDettImmobile(@PathVariable long id) {
+        return immobileService.findById(id);
+    }
+
+    //creazione immobile
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    @PreAuthorize("hasAnyAuthority('ADMIN')")
+    public Immobile nuovoImmobile(@RequestBody @Validated NewImmoPayload body, BindingResult valRes) {
+        if (valRes.hasErrors()) {
+            List<String> errList = new ArrayList<>();
+            for (int i = 0; i < valRes.getFieldErrors().size(); i++) {
+                errList.add(valRes.getFieldErrors().get(i).getDefaultMessage());
+            }
+            throw new ValidazioneFallitaExeption(errList);
+        }
+        Immobile i = immobileService.salvaImmobile(body);
+        return i;
+    }
+
+    //cancellazione immobile
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @PreAuthorize("hasAnyAuthority('ADMIN')")
+    public void cancellaImmobile(@PathVariable long id) {
+        immobileService.cancellaImmobile(id);
+    }
+
+
+}
